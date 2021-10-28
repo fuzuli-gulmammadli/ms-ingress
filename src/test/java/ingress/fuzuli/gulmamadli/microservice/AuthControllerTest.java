8f515a36cd4e9c381e8fdfaf0d70a9bf371433f5
fuzuli-gulmammadli <fuzuligulmammadli@gmail.com>
2021-10-28 17:54:46 +0400

    edit

--- src/test/java/ingress/fuzuli/gulmamadli/microservice/AuthControllerTest.java
+++ src/test/java/ingress/fuzuli/gulmamadli/microservice/AuthControllerTest.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import ingress.fuzuli.gulmamadli.microservice.controller.AuthController;
 import ingress.fuzuli.gulmamadli.microservice.dto.SignUpDto;
 import ingress.fuzuli.gulmamadli.microservice.exception.EmptyInputException;
+import ingress.fuzuli.gulmamadli.microservice.exception.InvalidEmailFormatException;
+import ingress.fuzuli.gulmamadli.microservice.exception.InvalidPasswordException;
+import ingress.fuzuli.gulmamadli.microservice.exception.PasswordsDoNotMatchException;
 import ingress.fuzuli.gulmamadli.microservice.service.AuthService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -55,6 +58,75 @@ public class AuthControllerTest {
         ).andExpect(status().isBadRequest())
                 .andExpect(content().string(containsString("email")))
                 .andExpect(content().string(containsString("must not be null")));
+    }
+
+    @Test
+    void whenInvalidEmailThenBadRequest() throws Exception {
+
+        //Arrange
+        SignUpDto signUpDto = new SignUpDto();
+        signUpDto.setEmail("invalid-email");
+        signUpDto.setPassword("12345678");
+        signUpDto.setPasswordConf("12345678");
+        signUpDto.setPrivacyPolicy(true);
+
+        InvalidEmailFormatException invalidEmailFormatException = new InvalidEmailFormatException("/v1/auth/sign-up");
+        when(authService.signUpUser(signUpDto)).thenThrow(invalidEmailFormatException);
+
+        //Act
+        mockMvc.perform(
+                post("/v1/auth/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signUpDto))
+        ).andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString("email")))
+                .andExpect(content().string(containsString("must be a well-formed email address")));
+    }
+
+    @Test
+    void whenInvalidPasswordThenBadRequest() throws Exception {
+
+        //Arrange
+        SignUpDto signUpDto = new SignUpDto();
+        signUpDto.setEmail("fuzuli@mail.com");
+        signUpDto.setPassword("12345");
+        signUpDto.setPasswordConf("12345");
+        signUpDto.setPrivacyPolicy(true);
+
+        InvalidPasswordException invalidPasswordException = new InvalidPasswordException("/v1/auth/sign-up");
+        when(authService.signUpUser(signUpDto)).thenThrow(invalidPasswordException);
+
+        //Act
+        mockMvc.perform(
+                post("/v1/auth/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signUpDto))
+        ).andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString("password")))
+                .andExpect(content().string(containsString("size must be between 8 and 30")));
+    }
+
+    @Test
+    void whenPasswordsDoNotMatchThenBadRequest() throws Exception {
+
+        //Arrange
+        SignUpDto signUpDto = new SignUpDto();
+        signUpDto.setEmail("fuzuli@mail.com");
+        signUpDto.setPassword("12345678");
+        signUpDto.setPasswordConf("12345679");
+        signUpDto.setPrivacyPolicy(true);
+
+        PasswordsDoNotMatchException passwordsDoNotMatchException = new PasswordsDoNotMatchException("/v1/auth/sign-up");
+        when(authService.signUpUser(signUpDto)).thenThrow(passwordsDoNotMatchException);
+
+        //Act
+        mockMvc.perform(
+                post("/v1/auth/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signUpDto))
+        ).andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString("password")))
+                .andExpect(content().string(containsString("Password do not match")));
     }
 
 }
